5a7c41c0734e5c316f9e9b22e53aa9e367cf8d93
Marcel Meijer <47677900+meijermarcel@users.noreply.github.com>
2024-09-03 21:32:07 -0400

    feat: angular material paginator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,9 +12,11 @@ import { dashboardFeatureKey, dashboardReducer } from './store/reducers';
 import { provideHttpClient } from '@angular/common/http';
 import { ItemsOnSaleComponent } from './pages/dashboard/components/items-on-sale/items-on-sale.component';
 import { ItemInfoComponent } from './pages/dashboard/components/item-info/item-info.component';
+import { MatPaginatorModule } from '@angular/material/paginator';
+
 @NgModule({
     declarations: [AppComponent, DashboardComponent, ItemsOnSaleComponent, ItemInfoComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    imports: [BrowserModule, AppRoutingModule, MatPaginatorModule],
     providers: [
         provideHttpClient(),
         provideStore({
--- src/app/pages/dashboard/components/items-on-sale/items-on-sale.component.ts
+++ src/app/pages/dashboard/components/items-on-sale/items-on-sale.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input } from '@angular/core';
 import { Item } from '../../../../store/state';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
     selector: 'items-on-sale',
@@ -12,7 +13,7 @@ export class ItemsOnSaleComponent {
 
     itemsFiltered: Item[] = [];
     itemsForPage: Item[] = [];
-    itemsPerPage = 5;
+    pageSize = 5;
 
     constructor(private cd: ChangeDetectorRef) {}
 
@@ -27,9 +28,21 @@ export class ItemsOnSaleComponent {
                 item.currentPrice !== item.averagePrice &&
                 item.currentDiffFromAvgPrice < 0
         );
-        console.log(this.itemsFiltered);
-        this.itemsForPage = this.itemsFiltered.slice(0, this.itemsPerPage);
 
+        // sort by currentDiffFromAvgPrice ascending
+        this.itemsFiltered.sort((a, b) => {
+            return a.currentDiffFromAvgPrice! - b.currentDiffFromAvgPrice!;
+        });
+        console.log(this.itemsFiltered);
+        this.itemsForPage = this.itemsFiltered.slice(0, this.pageSize);
+
+        this.cd.markForCheck();
+    }
+
+    handlePageEvent(event: PageEvent) {
+        const startIndex = event.pageIndex * event.pageSize;
+        const endIndex = startIndex + event.pageSize;
+        this.itemsForPage = this.itemsFiltered.slice(startIndex, endIndex);
         this.cd.markForCheck();
     }
 }
